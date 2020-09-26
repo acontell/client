e3dd9748c47b02c7604104452a67386b9d2deee7
acontell <arcadio.contell@avoristravel.com>
2020-09-26 11:57:01 +0200

    - Added usage of clients in app

--- feign-client/src/main/java/com/example/client/FeignClientAutoConfiguration.java
+++ feign-client/src/main/java/com/example/client/FeignClientAutoConfiguration.java
@@ -1,11 +1,12 @@
 package com.example.client;
 
 import com.example.client.clients.SampleClient;
+import com.example.client.clients.SampleClientWithConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@EnableFeignClients(clients = SampleClient.class)
+@EnableFeignClients(clients = {SampleClient.class, SampleClientWithConfiguration.class})
 @Configuration
 @ComponentScan
 public class FeignClientAutoConfiguration {
--- sample-app/src/main/java/com/example/sample/app/controllers/TestController.java
+++ sample-app/src/main/java/com/example/sample/app/controllers/TestController.java
@@ -1,5 +1,8 @@
 package com.example.sample.app.controllers;
 
+import com.example.client.clients.SampleClientWithConfiguration;
+import com.example.client.models.Request;
+import com.example.client.models.Response;
 import com.example.client.services.SampleClientService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,15 +11,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     private final SampleClientService sampleClientService;
+    private final SampleClientWithConfiguration client;
 
-    TestController(final SampleClientService sampleClientService) {
+    TestController(final SampleClientService sampleClientService,
+                   final SampleClientWithConfiguration client) {
 
         this.sampleClientService = sampleClientService;
+        this.client = client;
     }
 
     @GetMapping("/test-client")
     public String getResponse() {
 
         return this.sampleClientService.searchCustomerCostCenter("HELLO");
+    }
+
+    @GetMapping("/test-client-with-configuration")
+    public Response getResponseWithConfiguration() {
+
+        return this.client.searchCustomerBusiness(new Request());
     }
 }
